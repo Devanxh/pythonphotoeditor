6b0511a40a0db6a6ab9ac9fdbc5a92fa50180122
Devansh <73755529+Devanxh@users.noreply.github.com>
2022-03-23 16:44:22 +0530

    Update effects.py

--- effects.py
+++ effects.py
@@ -8,10 +8,21 @@ def brightness(image, factor):
     new_image = Image(x_pixels=x_pixels, y_pixels=y_pixels,num_channels=num_channels)
     new_image.array = image.array*factor
     return new_image
+
+# def combine_images(image1,image2):
+#     x_pixels, y_pixels, num_channels = image1.array.shape
+#     new_image = Image(x_pixels=x_pixels, y_pixels=y_pixels,num_channels=num_channels)
+#     # for x in range(x_pixels):
+#     #     for y in range(y_pixels):
+#     #         for z in range()
+#     new_image.array[x_pixels,y_pixels,num_channels]=(image1.array[x_pixels,y_pixels,num_channels]+image2.array[x_pixels,y_pixels,num_channels])**0.5
+#     return new_image
     
 
 if __name__ == '__main__':
     sky = Image(filename='sky1.png')
-    brightened_im = brightness(sky,1.7)
-    brightened_im.write_image('brd.png')
-    #city = Image(filename='city.png')
+    # moto = Image(filename='moto.png')
+    # brightened_im = brightness(sky,1.7)
+    # brightened_im.write_image('brd.png')
+    # combine_image = combine_images(sky,moto)
+    # combine_image.write_image('nice.png')
